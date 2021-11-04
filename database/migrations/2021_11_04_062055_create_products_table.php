7b642c0e2d47ba1c5ea2c45d90dad691ec94ff21
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2021-11-04 14:26:02 +0800

    Created products table

---
+++ database/migrations/2021_11_04_062055_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigInteger('store_id');
+            $table->decimal('price', 10, 2);
+            $table->bigInteger('season_id');
+            $table->foreign('store_id')->references('id')->on('stores');
+            $table->foreign('season_id')->references('id')->on('seasons');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
