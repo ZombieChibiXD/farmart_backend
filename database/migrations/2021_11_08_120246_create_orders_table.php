0896ba193e357b583c883e26ac88369a5ac0c48e
Gamdara <47179398+Gamdara@users.noreply.github.com>
2021-11-08 20:08:27 +0800

    Create Migration for table Order

---
+++ database/migrations/2021_11_08_120246_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->bigInteger('total');
+            $table->bigInteger('status');
+            $table->text('dropoff_location');
+            $table->string('transaction_code',50);
+            $table->string('courrier_code',50);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
